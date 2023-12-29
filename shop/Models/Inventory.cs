e22e4e5dbc02d984e8fd2ffaaff5b391f97c3eb1
Abolfazl <shishegar.abalfazl@gmail.com>
2023-12-29 06:51:33 +0330

    Add changes to models

---
+++ shop/Models/Inventory.cs
@@ -0,0 +1,8 @@
+﻿namespace shop.Models
+{
+    public class Inventory
+    {
+        public string Color { get; set; }
+        public string Size { get; set; }
+    }
+}
--- shop/Models/Product.cs
+++ shop/Models/Product.cs
@@ -7,8 +7,6 @@
         public string Description { get; set; }
         public decimal Price { get; set; }
         public int StockQuantity { get; set; }
-        public string Color { get; set; }
-        public string Size { get; set; }
         public string ImageUrl { get; set; }
         public int CategoryId { get; set; }
     }
